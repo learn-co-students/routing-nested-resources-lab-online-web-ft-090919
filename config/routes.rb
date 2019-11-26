9ed199a923eabe02f8df95ec1b011382fcbedf53
averagealloy <m.mctighe10@gmail.com>
2019-11-26 12:51:47 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do 
+    resources :songs, only: [:index, :show]
+  end
   resources :songs
 end
+
+#for artist do song and then from there only do show and index 
